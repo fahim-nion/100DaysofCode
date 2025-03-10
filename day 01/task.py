fc420161a297d674b26431298e97eab4aebc7230
fahim-nion <fahimalhassan4@gmail.com>
2025-03-11 02:15:24 +0600

    first day

--- day 01/task.py
+++ day 01/task.py
@@ -1,5 +1,25 @@
 # print("Hello World")
 
-str1 = input("Where you were born?")
-str2 = input("What is your cats name?")
-print(f"Your band name can be {str1} {str2}")
+# str1 = input("Where you were born?\n")
+# str2 = input("What is your cats name?\n")
+# print(f"Your band name can be {str1} {str2}")
+
+
+
+
+#012 Variables
+# Instructions
+# We have 2 variables glass1 and glass2. glass1 contains milk and glass2 contains juice. Write 3 lines of code to switch the contents of the variables. You are not allowed to type the words "milk" or "juice". You are only allowed to use variables to solve this exercise.
+
+
+#solve
+
+glass1 = "milk"
+glass2 = "juice"
+
+
+temp = glass1
+glass1= glass2
+glass2 = temp
+
+print(f"Milk er glass e ekhon {glass1} Juice er glass e ekhon {glass2}")
