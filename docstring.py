a711489055dcce6f53ccb1d75b7aa0257402da83
fahim-nion <fahimalhassan4@gmail.com>
2026-03-28 03:22:29 +0600

    python

---
+++ docstring.py
@@ -0,0 +1,14 @@
+def a_Func(a,b):
+    '''this function sums a value'''
+    return a+b
+
+def b_Func(a,b):
+    '''this function multiplies a value'''
+    return a*b
+
+# print(a_Func(5,7))
+
+print(a_Func.__doc__)
+print(b_Func.__doc__)
+
+#prints "this function sums a value"
