50e21749b522a79e82b23b80e32fe9a9df1f107e
fahim-nion <fahimalhassan4@gmail.com>
2026-04-20 22:46:18 +0600

    updated python learning

--- python_io.py
+++ python_io.py
@@ -8,6 +8,8 @@
 "x" - [Exclusive creation] if file already exists then it will fail, but if the file does not exists then it will create one.
 "a" - adds more content to a file
 "t" - text mode
+"b" - binary mode
+"+" - read + write both
 
 
 
