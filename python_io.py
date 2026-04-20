ca5e852a4f1b8a50fd6150510ec68718eb1c974f
fahim-nion <fahimalhassan4@gmail.com>
2026-04-20 22:45:06 +0600

    updated python learning

---
+++ python_io.py
@@ -0,0 +1,16 @@
+# comment
+# File IO basics
+
+'''
+
+"r" - Open file for reading
+"w" - Open file for writting
+"x" - [Exclusive creation] if file already exists then it will fail, but if the file does not exists then it will create one.
+"a" - adds more content to a file
+"t" - text mode
+
+
+
+'''
+
+
