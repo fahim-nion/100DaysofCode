f8a40433aa21a49e4d3a84f4530713db7b722277
fahim-nion <fahimalhassan4@gmail.com>
2026-04-20 22:48:53 +0600

    updated python learning

--- python_io.py
+++ python_io.py
@@ -3,16 +3,19 @@
 
 '''
 
-"r" - Open file for reading
+"r" - Open file for reading {default mode}
 "w" - Open file for writting
 "x" - [Exclusive creation] if file already exists then it will fail, but if the file does not exists then it will create one.
 "a" - adds more content to a file
-"t" - text mode
+"t" - text mode {default mode}
 "b" - binary mode
 "+" - read + write both
 
 
 
 '''
+
+
+
 
 
