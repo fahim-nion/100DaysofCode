ce47f08acd6dd39bf2a303793bb34fc1c4977851
fahim-nion <fahimalhassan4@gmail.com>
2026-04-22 13:56:17 +0600

    updated learning

--- c-reading_file.py
+++ c-reading_file.py
@@ -1,3 +1,24 @@
-with open('pi.txt') as f:
-    contents = f.read()
-    print(contents)
+# with open('pi.txt') as f:
+#     contents = f.read()
+#     print(contents)
+    
+#reading line by line
+
+with open("pi.txt") as f:
+    for line in f:
+        print(line)
+
+
+'''
+if the file is in another folder that needs access then it should be with backslashes. 
+
+with open('project\\file-1\\desired-file') as f:
+
+working with relative file path is also a solution 
+
+file_path = 'C:\\Users\\ehmatthes\\other_files\\text_files\\filename.txt'
+with open(file_path) as file_object:
+
+
+
+'''
