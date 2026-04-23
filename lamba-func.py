cf75b20b9fd2695008292cccafb626a89d0934cb
fahim-nion <fahimalhassan4@gmail.com>
2026-04-24 00:12:23 +0600

    learning lambda function

---
+++ lamba-func.py
@@ -0,0 +1,21 @@
+#lambda function is for convenience
+
+#also called anonymous function
+
+
+# def add(a,b):
+#     return a+b
+
+
+add = lambda x,y: x+y
+
+minus = lambda x, y: x-y
+
+
+# def minus (x,y):
+#     return x-y
+
+#these both are the same thing
+
+print(add(2,4))
+print(minus(6,1))
