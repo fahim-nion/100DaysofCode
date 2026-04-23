c3d5a0168a74a2fc2edc44629f40238637b9af24
fahim-nion <fahimalhassan4@gmail.com>
2026-04-24 02:15:08 +0600

    updating learning

---
+++ time-module.py
@@ -0,0 +1,20 @@
+import time
+
+# initial = time.time()
+# # print(initial)
+# k= 0
+
+# while (k<=99):
+#     print("Hi")
+#     k+=1
+# print(f"While loop took {time.time()-initial}")
+
+# initial2= time.time()
+
+# for i in range(100):
+#     print("I am for loop")
+# print(f"For loop took {time.time()-initial2}")
+
+
+localTime = time.asctime(time.localtime(time.time()))
+print(localTime)
