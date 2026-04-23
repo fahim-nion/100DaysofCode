d64463c665810adba197c7896139ef9136713cdf
fahim-nion <fahimalhassan4@gmail.com>
2026-04-24 00:51:54 +0600

    learning lambda function

--- using-module.py
+++ using-module.py
@@ -1,1 +1,9 @@
--
+import random
+# print(random.randint(1,10))
+
+random_num = random.random()
+print(random_num)
+
+myl = ["space-toon","cartoon network","history", "discovery"]
+
+print(random.choice(myl))
