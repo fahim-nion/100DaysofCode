3f51e7a6840ed6172882858ec6efe5372dfd6e68
fahim-nion <fahimalhassan4@gmail.com>
2026-04-25 17:20:41 +0600

    updated learning

--- OOP-7.py
+++ OOP-7.py
@@ -26,8 +26,15 @@ class Employee:
         return(f"This is a good string executed with {string}")
 
 class Programmer(Employee):
+    def __init__(self, name, sal, role,lang):
+        self.name = name
+        self.salary = sal
+        self.role = role
+        self.language = lang
+        
+        #can be used super() here
     def printProg(self):
-        return f"Programmer Name is {self.name}. Salary is {self.salary} and role is {self.role}"
+        return f"Programmer Name is {self.name}. Salary is {self.salary} and role is {self.role} and knows {self.language}"
 
 
 
@@ -35,8 +42,8 @@ harry = Employee("Harry",5000,"Instructor")
 
 rohan = Employee("Rohan",3000,"Student")
 
-shubham = Programmer("Shubham", 8000, "Programmer")
-karan = Programmer("Karan", 10000, "Programmer")
+shubham = Programmer("Shubham", 8000, "Programmer",["C","JavaScript"])
+karan = Programmer("Karan", 10000, "Programmer",["C","Python","Dart"])
 
 
 # print(rohan.printGood("wow string"))
