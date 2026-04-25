f197c834416ba92bcdf39798581e65e95fdf8481
fahim-nion <fahimalhassan4@gmail.com>
2026-04-25 15:59:44 +0600

    updated learning

--- OOP-3.py
+++ OOP-3.py
@@ -3,20 +3,26 @@
 
 class Employee:
     num_of_leaves = 8
-    
+    #Constructor
+    def __init__(self, name, sal, role):
+        self.name = name
+        self.salary = sal
+        self.role = role
+    #this is method
     def printDetails(self):
         return f"Name is {self.name}. Salary is {self.salary} and role is {self.role}"
 
-harry = Employee()
+harry = Employee("Harry",5000,"Instructor")
 
-rohan = Employee()
+rohan = Employee("Rohan",3000,"Student")
 
-harry.name = "Harry"
-harry.salary = 5000
-harry.role = "Instructor"
+# harry.name = "Harry"
+# harry.salary = "Harry"
+# harry.role = "Instructor"
 
-rohan.name = "Rohan"
-rohan.salary = 3000
-rohan.role = "Student"
+# rohan.name = "Rohan"
+# rohan.salary = 3000
+# rohan.role = "Student"
 
 print(rohan.printDetails())
+print(harry.printDetails())
