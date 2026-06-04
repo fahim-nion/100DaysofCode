1fd23ecfee8721c895de76d627a51bc1164ab816
fahim-nion <fahimalhassan4@gmail.com>
2026-06-04 23:44:20 +0600

    learning Numpy

--- numpy-learning/starting-off.py
+++ numpy-learning/starting-off.py
@@ -2,5 +2,13 @@ import numpy as np
 
 arr = np.array([1,2,3,4,5,60], dtype=int)
 
-print(arr)
-print(type(arr))
+# print(arr)
+# print(type(arr))
+
+#2D array
+
+
+arr2 = np.array([[1,2,3],[4,5,6]])
+
+print(arr2)
+print(type(arr2))
