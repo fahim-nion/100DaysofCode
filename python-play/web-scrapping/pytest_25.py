40c10c2e63bdb55386091ab978f9ee87a540e273
fahim-nion <fahimalhassan4@gmail.com>
2026-07-11 00:50:45 +0600

    learning pytest

---
+++ python-play/web-scrapping/pytest_25.py
@@ -0,0 +1,13 @@
+#pytest Fixtures
+import pytest
+
+
+@pytest.fixture(scope = "module")
+def preWork():
+    print("I set up browser instance!")
+
+def test_initialCheck(preWork):
+    print("Thid is first Test")
+    
+def test_scndCheck(preWork):
+    print("Thid is second Test")
